2ad247256fdccc9ec7f584c165538165ab726f94
[genie-ru] <[genie.zgmf987@gmail.com]>
2024-12-10 03:37:44 +0900

    add route page

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>portfolio_page</div>
+  )
+}
+
+export default page
---
+++ src/app/reports/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Reports_page</div>
+  )
+}
+
+export default page
